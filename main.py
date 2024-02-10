176598d211257d5f1816851dc71931ae668c0ee9
MaAri <MaAriec@yandex.ru>
2024-02-10 19:46:35 +0300

    Поиск по названию. Управление через стрелочки.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ MAP_API_SERVER = "http://static-maps.yandex.ru/1.x/"
 GEOCODER = "http://geocode-maps.yandex.ru/1.x/"
 MAP_FILENAME = 'map.png'
 
-#Управление сейчас производится с помощью клавиш WASD.
+#Управление сейчас производится со стрелочками.
 class MainWindow(QtWidgets.QMainWindow, MainWindow_Ui):
     def __init__(self):
         super().__init__()
@@ -34,6 +34,7 @@ class Map_Window(QtWidgets.QMainWindow):
         self.view = 'map'
         super().__init__()
         self.set_ui()
+        self.pt = ''
         self.get_map()
 
 
@@ -41,25 +42,27 @@ class Map_Window(QtWidgets.QMainWindow):
         self.label = QtWidgets.QLabel(self)
         self.label.resize(600, 450)
         self.resize(600, 450)
-        self.scheme = QtWidgets.QPushButton(self)
+        self.scheme = QtWidgets.QToolButton(self)
         self.scheme.setText('Схема')
         self.scheme.move(300, 420)
-        self.sputnic = QtWidgets.QPushButton(self)
+        self.sputnic = QtWidgets.QToolButton(self)
         self.sputnic.setText('Спутник')
         self.sputnic.move(400, 420)
-        self.hybride = QtWidgets.QPushButton(self)
+        self.hybride = QtWidgets.QToolButton(self)
         self.hybride.setText('Гибрид')
         self.hybride.move(500, 420)
         self.search_line = QtWidgets.QLineEdit(self)
         self.search_line.move(0, 420)
         self.search_line.resize(200, 30)
-        self.search_button = QtWidgets.QPushButton(self)
+        self.search_button = QtWidgets.QToolButton(self)
+
         self.search_button.setText('Искать')
         self.search_button.move(200, 420)
         self.search_button.clicked.connect(self.search)
         layout = QtWidgets.QHBoxLayout(self)
         layout.addWidget(self.label)
-        
+        self.setFocus()
+
         self.scheme.clicked.connect(lambda: self.change_view('map'))
         self.sputnic.clicked.connect(lambda: self.change_view('sat'))
         self.hybride.clicked.connect(lambda: self.change_view('sat,skl'))
@@ -74,13 +77,16 @@ class Map_Window(QtWidgets.QMainWindow):
         geocoder_params = {
             "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
             "geocode": self.search_line.text(),
-            "format": "json"}
+            "format": "json",
+            }
 
         response = requests.get(GEOCODER, params=geocoder_params)
         coords = response.json()["response"]["GeoObjectCollection"][
     "featureMember"][0]["GeoObject"]["Point"]["pos"].split(' ')
         self.long = coords[0]
         self.lat = coords[1]
+        self.pt = f'{self.long},{self.lat},pm2rdm'
+        self.setFocus()
         self.get_map()
 
 
@@ -88,7 +94,11 @@ class Map_Window(QtWidgets.QMainWindow):
     def get_map(self):
         params = {'ll': ','.join([self.long, self.lat]),
                   "l": self.view,
-                  'spn': ','.join(list(map(str, self.spn)))}
+                  'spn': ','.join(list(map(str, self.spn)))
+                  }
+        if self.pt:
+            params['pt'] = self.pt
+
         response = requests.get(MAP_API_SERVER, params=params)
         with open(MAP_FILENAME, 'wb') as file:
             file.write(response.content)
@@ -98,7 +108,9 @@ class Map_Window(QtWidgets.QMainWindow):
 
 
 
+
     def keyPressEvent(self, event):
+
         if event.key() == Qt.Key_PageUp:
             self.spn = [i - i / 2 for i in self.spn]
             self.get_map()
@@ -107,27 +119,27 @@ class Map_Window(QtWidgets.QMainWindow):
             if self.spn[0] > 90.0:
                 self.spn = [90.0, 90.0]
             self.get_map()
-        elif event.key() == Qt.Key_W:
+        elif event.key() == Qt.Key_Up:
             self.lat = str(float(self.lat) + self.spn[0])
 
             if float(self.lat) > 80.0:
                 self.lat = str(-80.0)
             self.get_map()
-        elif event.key() == Qt.Key_S:
+        elif event.key() == Qt.Key_Down:
             self.lat = str(float(self.lat) - self.spn[0])
             if float(self.lat) < -80.0:
                 self.lat = str(80.0)
 
             self.get_map()
 
-        elif event.key() == Qt.Key_A:
+        elif event.key() == Qt.Key_Left:
 
 
             self.long = str(float(self.long) - self.spn[0])
             if float(self.long) < -180.0:
                 self.long = str(180.0)
             self.get_map()
-        elif event.key() == Qt.Key_D:
+        elif event.key() == Qt.Key_Right:
 
             self.long = str(float(self.long) + self.spn[0])
             if float(self.long) > 180.0:
